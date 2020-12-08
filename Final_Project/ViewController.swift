1f07b1417702c6f39b06b604c2407d02b5fd26e3
garn0080 <54903674+garn0080@users.noreply.github.com>
2020-12-08 13:15:48 -0500

    Initial Commit

---
+++ Final_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final_Project
+//
+//  Created by Mariana garnica on 2020-12-08.
+//  Copyright © 2020 Algonquin College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
